4cc2607aec4341c6cea72229d62203c1e738b820
james <chengt20@uw.edu>
2022-08-12 22:15:49 -0700

    Initial Commit

---
+++ Shared/SodaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SodaApp.swift
+//  Shared
+//
+//  Created by James on 8/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SodaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
